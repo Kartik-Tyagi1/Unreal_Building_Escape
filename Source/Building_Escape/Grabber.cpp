eac735cd4cce9a59aecbdee8774ec24e78da1be3
Kartik-Tyagi1 <kartiktyagi278@gmail.com>
2021-07-14 17:16:26 -0400

    Added DefaultPawn Blueprint and grabber class

---
+++ Source/Building_Escape/Grabber.cpp
@@ -0,0 +1,49 @@
+// Copyright Michael Bridges 2019
+
+#include "Grabber.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h" // GetPlayerViewPoint()
+
+#define OUT // This is not functional, it is only for readability so it is easy to tell what variables are out variables 
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Reporting for Duty"));	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get player's viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation);
+
+	UE_LOG(LogTemp, Warning, TEXT("Location: %s , Rotation: %s"), 
+		*PlayerViewPointLocation.ToString(), 
+		*PlayerViewPointRotation.ToString());
+
+	// Ray Cast out to a certain distance
+
+	// See what the ray hits
+}
+
